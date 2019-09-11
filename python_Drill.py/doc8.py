7ae26a5170d204356f3375412ba41e3a73c7e91a
Fred Picard <50530374+FredPicard@users.noreply.github.com>
2019-09-10 17:09:32 -0700

    Added methods drill
    Added Listdir()method, path.join()method, and getmtime()method

---
+++ python_Drill.py/doc8.py
@@ -0,0 +1,165 @@
+
+
+import os
+import time
+
+path = "C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py"
+dir = os.listdir( path )
+
+# This would print all the files and directories
+for file in dir:
+   print(dir)
+   
+fName = "doc1.txt"
+fPath = "C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py"
+abPath = os.path.join(fPath, fName)
+print(abPath)
+path = 'C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py\\doc1.txt'
+modification_time = os.path.getmtime(path) 
+print("Last modification time since the epoch:", modification_time) 
+local_time = time.ctime(modification_time) 
+print("Last modification time(Local time)\\n:", local_time) 
+
+
+fName = "doc2.txt"
+fPath = "C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py"
+abPath = os.path.join(fPath, fName)
+print(abPath)
+path = 'C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py\\doc2.txt'
+modification_time = os.path.getmtime(path) 
+print("Last modification time since the epoch:", modification_time) 
+local_time = time.ctime(modification_time) 
+print("Last modification time(Local time):", local_time) 
+
+
+fName = "doc3.txt"
+fPath = "C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py"
+abPath = os.path.join(fPath, fName)
+print(abPath)
+path = 'C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py\\doc3.txt'
+modification_time = os.path.getmtime(path) 
+print("Last modification time since the epoch:", modification_time) 
+local_time = time.ctime(modification_time) 
+print("Last modification time(Local time):", local_time) 
+
+
+
+
+
+fName = "doc4.txt"
+fPath = "C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py"
+abPath = os.path.join(fPath, fName)
+print(abPath)
+path = 'C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py\\doc4.txt'
+modification_time = os.path.getmtime(path) 
+print("Last modification time since the epoch:", modification_time) 
+local_time = time.ctime(modification_time) 
+print("Last modification time(Local time):", local_time) 
+
+
+
+
+
+fName = "doc5.txt"
+fPath = "C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py"
+abPath = os.path.join(fPath, fName)
+print(abPath)
+path = 'C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py\\doc5.txt'
+modification_time = os.path.getmtime(path) 
+print("Last modification time since the epoch:", modification_time) 
+local_time = time.ctime(modification_time) 
+print("Last modification time(Local time):", local_time) 
+
+
+
+
+
+
+
+fName = "doc6.txt"
+fPath = "C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py"
+abPath = os.path.join(fPath, fName)
+print(abPath)
+path = 'C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py\\doc6.txt'
+modification_time = os.path.getmtime(path) 
+print("Last modification time since the epoch:", modification_time) 
+local_time = time.ctime(modification_time) 
+print("Last modification time(Local time):", local_time) 
+
+
+
+
+
+
+fName = "doc7.txt"
+fPath = "C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py"
+abPath = os.path.join(fPath, fName)
+print(abPath)
+path = 'C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py\\doc7.txt'
+modification_time = os.path.getmtime(path) 
+print("Last modification time since the epoch:", modification_time) 
+local_time = time.ctime(modification_time) 
+print("Last modification time(Local time):", local_time) 
+
+
+
+
+
+
+fName = "doc9.txt"
+fPath = "C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py"
+abPath = os.path.join(fPath, fName)
+print(abPath)
+path = 'C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py\\doc9.txt'
+modification_time = os.path.getmtime(path) 
+print("Last modification time since the epoch:", modification_time) 
+local_time = time.ctime(modification_time) 
+print("Last modification time(Local time):", local_time) 
+
+
+
+
+
+fName = "doc10.txt"
+fPath = "C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py"
+abPath = os.path.join(fPath, fName)
+print(abPath)
+path = 'C:\\Users\\Student\\Documents\\GitHub\\python_Drill.py\\doc10.txt'
+modification_time = os.path.getmtime(path) 
+print("Last modification time since the epoch:", modification_time) 
+local_time = time.ctime(modification_time) 
+print("Last modification time(Local time):", local_time) 
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
